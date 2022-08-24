f7e41f55985a8f120834b5b4cde5faba8aecab3d
Victor Alex <victoralexsander5200@gmail.com>
2022-08-24 11:04:57 -0300

    feat: ajustando algumas telas

--- src/app/login/cadastro-inicial-artista/cadastro-inicial-artista.component.ts
+++ src/app/login/cadastro-inicial-artista/cadastro-inicial-artista.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-cadastro-inicial-artista',
@@ -11,17 +13,30 @@ export class CadastroInicialArtistaComponent implements OnInit {
   form = new FormGroup({
     nome: new FormControl(null, [Validators.required]),
     cpf: new FormControl(null, [Validators.required]),
-    genero: new FormControl(null, [Validators.required]),
+    genero: new FormControl('masculino', [Validators.required]),
     email: new FormControl(null, [Validators.required]),
     nascimento: new FormControl(null, [Validators.required]),
-    vinculo: new FormControl(null, [Validators.required])
+    vinculo: new FormControl('autonomo', [Validators.required])
   });
 
-  constructor() { }
+  constructor(private router: Router, public toastController: ToastController) { }
 
   ngOnInit() {}
 
-  entrar() {
+  async entrar() {
+    if (this.form.invalid) {
+      this.form.markAsDirty();
+      this.form.markAsTouched();
+      return;
+    }
 
+    const toast = await this.toastController.create({
+      message: 'Cadastrado com sucesso.',
+      duration: 2000
+    });
+
+    await toast.present();
+
+    this.router.navigate(['/']);
   }
 }
